7d88268b7c72d5882e371f109d15790b3ab10a51
Namluuu <145118434+namluan@users.noreply.github.com>
2024-04-24 11:21:46 +0200

    Initial commit with bdd

---
+++ Banque V4 - Etudiants/Controleur/Mgr.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Banque.DAL;
+using Banque.Modele;
+
+namespace Banque.Controleur
+{
+    class Mgr
+    {
+
+        CompteDao cd = new CompteDao();
+        ClientDao cld = new ClientDao();
+
+    
+      
+
+        public List<Client> chargementClBD()
+        {
+
+            return(cld.getClients());
+
+        }
+
+        public List<Compte> chargementCBD( Client cl)
+        {
+
+            return cd.getComptes(cl);
+
+        }
+
+        
+
+        public void updateClient(Client c)
+        {
+
+            cld.updateClient(c);
+
+        }
+
+        public void updateSolde(Compte c)
+        {
+
+            cd.updateSolde(c);
+
+        }
+
+        public void updateDecouvert(CompteCourant c)
+        {
+
+            cd.updateDecouvert(c);
+
+        }
+
+
+    }
+}
---
+++ Banque V4 - Etudiants/DAL/ConnectionSql.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Banque.DAL
+{
+    class ConnexionSql
+    {
+        private static ConnexionSql connection = null;
+
+        private MySqlConnection oleCn;
+
+        private static readonly object mylock = new object();
+
+        private string connString;
+
+
+        private ConnexionSql(string unProvider, string uneDataBase, string unUid, string unMdp)
+        {
+
+
+            try
+            {
+                connString = "SERVER=" + unProvider + ";" + "DATABASE=" +
+                uneDataBase + ";" + "UID=" + unUid + ";" + "PASSWORD=" + unMdp + ";" ;
+                try
+                {
+                    oleCn = new MySqlConnection(connString);
+                }
+                catch (Exception emp)
+                {
+                    throw(emp);
+                }
+            }
+            catch (Exception emp)
+            {
+                throw (emp);
+            }
+
+
+
+        }
+
+
+
+        /**
+         * méthode de création d'une instance de connexion si elle n'existe pas (singleton)
+         */
+        public static ConnexionSql getInstance(string unProvider, string uneDataBase, string unUid, string unMdp)
+        {
+
+     //       lock ((mylock))
+        //    {
+
+                try
+                {
+
+
+                    if (null == connection)
+                    { // Premier appel
+                        connection = new ConnexionSql(unProvider, uneDataBase, unUid, unMdp);
+                  
+
+                    }
+
+                }
+                catch (Exception emp)
+                {
+                    throw(emp);
+
+
+                }
+                return connection;
+
+          //  }
+        }
+
+
+
+
+
+        /**
+         * Ouverture de la connexion
+         */
+        public void openConnection()
+        {
+
+            if (oleCn.State == System.Data.ConnectionState.Closed)
+                oleCn.Open();
+        }
+
+        //Close connection
+        public void closeConnection()
+        {
+            if (oleCn.State == System.Data.ConnectionState.Open)
+                oleCn.Close();
+        }
+
+
+        public bool isOpen()
+        {
+            bool res = false;
+          if  (oleCn.State == System.Data.ConnectionState.Open) { res = true; }
+
+            return (res);
+        }
+
+        /**
+         * Fermeture de la connexion
+         */
+       
+
+        /**
+         * Exécutiuon d'une requête
+         */
+        public MySqlCommand reqExec(string req)
+        {
+            MySqlCommand mysqlCom = new MySqlCommand(req, this.oleCn);
+            return (mysqlCom);
+        }
+
+    }
+}
